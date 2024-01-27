58bdb4f6a8f9d594e383d0687c3203a196e3c97b
KyLLlHEP <kushnerartur@gmail.com>
2024-01-27 15:14:32 +0000

    8th 8ku

--- index.js
+++ index.js
@@ -116,3 +116,20 @@ function sumTwoSmallestNumbers(numbers) {
 
   return sum;
 }
+//
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      // Check for division by zero
+      return value2 !== 0 ? value1 / value2 : undefined;
+    default:
+      // Handle invalid operation
+      return undefined;
+  }
+}
