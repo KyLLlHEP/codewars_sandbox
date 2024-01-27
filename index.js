d29f92ae85446e022c5baea3da3aaa1c24ce1350
KyLLlHEP <kushnerartur@gmail.com>
2024-01-27 15:18:29 +0000

    10th task 8ku

--- index.js
+++ index.js
@@ -146,3 +146,17 @@ function squareSum(numbers) {
 
   return sum;
 }
+//10th 8ku
+const rps = (p1, p2) => {
+  if (p1 === p2) {
+    return "Draw!";
+  } else if (
+    (p1 === "rock" && p2 === "scissors") ||
+    (p1 === "scissors" && p2 === "paper") ||
+    (p1 === "paper" && p2 === "rock")
+  ) {
+    return "Player 1 won!";
+  } else {
+    return "Player 2 won!";
+  }
+};
